0531d6d900bc53f3e6eed022a07d242db0a1c7a3
Kayne Ruse <kayneruse@gmail.com>
2019-10-27 13:24:05 +1100

    Added refresh-persistence

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,9 +6,20 @@ import reducers from './reducers/reducers.js';
 
 import App from './app.jsx';
 
+//persistence between page reloads
+const ITEM_NAME = 'state.marketplace';
+let initialState = localStorage.getItem(ITEM_NAME);
+initialState = initialState ? JSON.parse(initialState) : reducers({}, {type: null});
+
+const onDispatch = (state, action) => {
+	const ret = reducers(state, action);
+	localStorage.setItem(ITEM_NAME, JSON.stringify(ret));
+	return ret;
+}
+
 //start the process
 ReactDOM.render(
-	<AppProvider initialState={ reducers({}, {type: null}) } reducer={reducers}>
+	<AppProvider initialState={initialState} reducer={onDispatch}>
 		<App />
 	</AppProvider>,
 	document.querySelector("#root")
